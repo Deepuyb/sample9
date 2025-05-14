5ee12f75440f46af553ada5107686c5250494b5e
Deepuyb <deepugowda1162004@gmail.com>
2025-05-14 09:49:08 +0530

    Initial commit

---
+++ sample9/src/main/java/com/Deepuyb/App.java
@@ -0,0 +1,13 @@
+package com.Deepuyb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
